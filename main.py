b8ba7c250c39261f7e8316234cb2981b2d514df6
Antonio Rodriguez <antonioinorge@hotmail.com>
2024-11-12 15:31:53 +0100

    add response and soup

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-user_input = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:: ")  
+# user_input = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:: ")
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+# URL = f"https://www.billboard.com/charts/hot-100/{user_input}/"
+URL = "https://www.billboard.com/charts/hot-100/2024-08-03/"
+
+
+response = requests.get(URL, headers = header)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+print(soup.prettify())
